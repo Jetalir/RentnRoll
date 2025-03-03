fa80e970c90d89883d1ea38d8261faaa07da5ddd
Jetlir Kuci <jetalir@hotmail.com>
2025-03-03 22:56:49 +0100

    Fixed Index and Added Edit/delete buttons

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,15 +1,23 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RentnRoll.Data;
 
 namespace RentnRoll.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+		private readonly AppDbContext _context;
+		public IndexModel(AppDbContext context)
         {
+			_context = context;
+		}
 
-        }
+		public List<Vehicle> Vehicle { get; set; }
+		public void OnGet()
+        {
+            Vehicle = _context.Vehicles.ToList();
+		}
 
         public void OnPost()
         {
